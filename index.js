83b273235b2722d2b34f87669eed41e9f1f0dfb7
Rohit Shinde <rohitshinde0550@gmail.com>
2025-02-16 23:13:15 +0530

    Updates

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ import { fileURLToPath } from "url";
 var __filename = fileURLToPath(import.meta.url);
 var __dirname = dirname(__filename);
 var vite_config_default = defineConfig({
+  base: "/ResumeRohitShinde/",
   plugins: [react(), runtimeErrorOverlay(), themePlugin()],
   resolve: {
     alias: {
