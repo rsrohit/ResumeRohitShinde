61806b02b3527e998b5870c9d3a7c9244c75b09f
Rohit Shinde <rohitshinde0550@gmail.com>
2025-02-16 23:06:18 +0530

    Updates

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ var vite_config_default = defineConfig({
   },
   root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
+    outDir: path.resolve(__dirname, "dist"),
     emptyOutDir: true
   }
 });
